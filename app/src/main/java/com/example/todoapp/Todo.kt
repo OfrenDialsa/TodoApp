5690736530dbe5c8f7eec4c726613fe9f5508eeb
Nero24 <lolilolipo24@gmail.com>
2024-09-07 01:12:27 +0700

    Upload

---
+++ app/src/main/java/com/example/todoapp/Todo.kt
@@ -0,0 +1,24 @@
+package com.example.todoapp
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import java.time.Instant
+import java.util.Date
+
+data class Todo(
+    var id : Int,
+    var title : String,
+    var CreatedAt : Date
+)
+
+fun getFakeTodo() : List<Todo>{
+    return listOf<Todo>(
+        Todo(1, "First Todo", Date.from(Instant.now())),
+        Todo(2, "Second Todo", Date.from(Instant.now())),
+        Todo(3, "Third Todo", Date.from(Instant.now())),
+        Todo(4, "Fourth Todo", Date.from(Instant.now()))
+    )
+}
+
+
